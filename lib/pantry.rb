78ffa3dcd5236bfa8fde638808a11e1edb2ac661
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-25 13:21:22 -0600

    Initial Commit - complete on Iteration 1

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,20 @@
 class Pantry
+  
+  attr_reader       :stock 
+  def initialize
+    @stock = {}
+  end 
+  
+  def stock_check(item)
+    if @stock[item] != nil 
+      @stock[item]
+    else
+      0
+    end 
+  end 
+  
+  def restock(item, count)
+    @stock[item] = stock_check(item) + count 
+  end 
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,34 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  
+  def test_it_exists
+    pantry = Pantry.new 
+    
+    assert_instance_of Pantry, pantry 
+  end 
+  
+  def test_it_has_a_stock_hash
+    pantry = Pantry.new 
+  
+    assert_equal ({}), pantry.stock
+  end 
+  
+  def test_it_can_check_stock
+    pantry = Pantry.new 
+    
+    assert_equal 0, pantry.stock_check("Cheese")
+  end 
+  
+  def test_it_can_restock
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10) 
+    
+    assert_equal 10, pantry.stock_check("Cheese")
+    
+    pantry.restock("Cheese", 20)
+    
+    assert_equal 30, pantry.stock_check("Cheese")
+  end 
 
 end
